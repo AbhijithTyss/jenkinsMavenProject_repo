82de6298e70cb86a717aff9330e905026623118f
Abhijith1998 <123438717+Abhijith1998@users.noreply.github.com>
2023-12-19 12:41:15 +0530

    OnPollScmTest executed

---
+++ jenkinsMavenProject/src/test/java/jenkins/OnPollScmTest.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollScmTest {
+
+	@Test
+	public void onPoll() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		Reporter.log("OnPollScmTest executed",true);
+	}
+}
